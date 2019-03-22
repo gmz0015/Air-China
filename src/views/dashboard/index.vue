27820f1c5ac2fbf582ebdefc788d151d5afdd033
Mingze Gao <mgao14@sheffield.ac.uk>
2019-03-22 21:01:41 +0000

    Apply ECharts

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,33 +1,98 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name:{{ name }}</div>
-    <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
+  <div class="dashboard">
+
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <mapChart :chart-data="lineChartData"/>
+    </el-row>
+
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <raddar-chart/>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <pie-chart/>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <bar-chart/>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<script src="echarts.min.js"></script>
 <script>
-import { mapGetters } from 'vuex'
+// import { mapGetters } from 'vuex'
 import echarts from 'echarts'
+import mapChart from './components/MapChart'
+import RaddarChart from './components/RaddarChart'
+import PieChart from './components/PieChart'
+import BarChart from './components/BarChart'
+
+const mapChartData = {
+  newVisitis: {
+    latitude: [3.2, 24.2, 34, 44, 54, 64, 74],
+    longitude: [3.2, 24.2, 34, 44, 54, 64, 74]
+  }
+}
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
 export default {
   name: 'Dashboard',
+  components: {
+    RaddarChart,
+    PieChart,
+    BarChart,
+    mapChart
+  },
+  data () {
+    return {
+    }
+  },
+  created(){
+  },
+  method: {
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData["messages"]
+    }
+  },
   computed: {
-    ...mapGetters([
-      'name',
-      'roles'
-    ])
+  },
+  mounted() {
   }
 }
 </script>
 
-<style rel="stylesheet/scss" lang="scss" scoped>
+<style >
 .dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
   }
 }
 </style>
